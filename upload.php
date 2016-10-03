9d5dc89daa4192487f134c3d42ab90e736ab66ab
ChenMT <gene31105@gmail.com>
2016-10-02 18:13:38 -0700

    add

--- upload.php
+++ upload.php
@@ -17,7 +17,7 @@ if(isset($_POST["fileToUpload"])) {
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title></title>
+		<title>Upload</title>
 		<link rel="stylesheet" href="">
 	</head>
 	<body>
